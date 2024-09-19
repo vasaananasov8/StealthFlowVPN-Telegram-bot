dacc496b1ee9f0ba20433dc6c944887157d97b44
DKowalski25 <dshinoda25@gmail.com>
2024-09-19 20:29:07 +0300

    STEAL-7 -- Added subscription exceptions.

--- src/services/storage/repository/exceptions.py
+++ src/services/storage/repository/exceptions.py
@@ -4,6 +4,7 @@ class RepositoryException(Exception):
 
 class RepositoryUserNotFound(RepositoryException):
     """Исключение, которое возникает, когда пользователь не найден."""
+
     def __init__(self, user_id: int):
         self.user_id = user_id
         super().__init__(f'User with ID {self.user_id} not found.')
@@ -12,3 +13,16 @@ class RepositoryUserNotFound(RepositoryException):
 class RepositoryUserCreationError(RepositoryException):
     """Исключение для ошибок при создании пользователя"""
     ...
+
+
+class RepositorySubscriptionCreationError(RepositoryException):
+    """Исключение для ошибок при создании подписки"""
+    ...
+
+
+class RepositorySubscriptionNotFound(RepositoryException):
+    """Исключение, которое возникает, когда подписка не найдена."""
+
+    def __init__(self, subscription_id: int):
+        self.subscription_id = subscription_id
+        super().__init__(f'Subscription with ID {self.subscription_id} not found.')
