118f3dff79ac30c096f5a7c8461962f46421db2f
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-20 19:32:38 +0300

    STEAL-8: get vpn handler

--- src/bot/routers/user/get_vpn.py
+++ src/bot/routers/user/get_vpn.py
@@ -1,87 +1,113 @@
-import uuid
 from logging import getLogger
 from aiogram import Router, types
-from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from dependency_injector.wiring import inject, Provide
-from win32ctypes.pywin32.pywintypes import datetime
 
-from src.bot.ui import commands
+from src.bot.routers.fsm import UsePromo
+from src.bot.routers.utils import send_error_message
 from src.bot.ui.inline_keyboard import callbacks
-from src.bot.ui.inline_keyboard.keyboard import get_vpm_meny
+from src.bot.ui.inline_keyboard import keyboard
 from src.bot.ui.scripts import Scripts
 from src.container import AppContainer
-from src.core.models.subscription import Subscription
-from src.services.storage.infrastructure.interfaces.i_subscription_manager import ISubscriptionStorageManager
-from src.services.storage.infrastructure.interfaces.i_user_storage_manager import IUserStorageManager
-from src.services.vpn.i_vpn_manager import IVpnManager
+from src.services.storage.infrastructure.interfaces.i_promo_manager import IPromoManager, PromoCheckValue
+from src.services.vpn.i_vpn_manager import IVpnManager, ApplyPromoResultValues, ApplyPromoResult
 
 logger = getLogger(__name__)
 router = Router(name=__name__)
 
 
-@router.message(Command(commands.GET_VPN))
-@router.callback_query(lambda x: x == callbacks.GET_VPN)
+@router.callback_query(lambda x: x.data == callbacks.GET_VPN)
 # @handel_unexpected_exception
 @inject
 async def get_vpn(
-        msg: types.Message | None = None,
         callback: types.CallbackQuery | None = None,
-        subscription_storage_manager: ISubscriptionStorageManager = Provide[AppContainer.subscription_storage_manager],
-        vpn_manager: IVpnManager = Provide[AppContainer.vpn_handler],
-        scripts_handler: Scripts = Provide[AppContainer.handler_scripts]
 ) -> None:
-    kb = get_vpm_meny()
-    if msg is not None:
-        await msg.answer(text="stub", reply_markup=kb.as_markup())
-    else:
-        await callback.message.answer(text="stub", reply_markup=kb.as_markup())
+    """Open menu to get vpn. Now 2 options: 'Pay for vpn', 'Use promo'"""
+    kb = keyboard.get_vpm_meny()
+    await callback.message.answer(text="stub", reply_markup=kb.as_markup())
+    return
 
-@router.callback_query(lambda x: x == callbacks.USER_PROMO)
-@inject
-async def user_promo(
+
+@router.callback_query(lambda x: x.data == callbacks.USER_PROMO)
+async def use_promo(
         callback: types.CallbackQuery,
+        state: FSMContext,
 ) -> None:
-    ...
-    # TODO: Слой бизнес логики для впн
+    """Start fsm to user promo"""
+    await callback.message.answer(text="Введите промокод")
+    await state.set_state(UsePromo.set_promo)
+    return
 
-    # user_id = msg.from_user.id if msg is not None else callback.from_user.id
-    # username = msg.from_user.username if msg is not None else callback.from_user.username
 
+@router.message(UsePromo.set_promo)
+@inject
+async def set_promo(
+        msg: types.Message,
+        state: FSMContext,
+        promo_manager: IPromoManager = Provide[AppContainer.promo_manager],
+        scripts_handler: Scripts = Provide[AppContainer.handler_scripts],
+        vpn_manager: IVpnManager = Provide[AppContainer.vpn_manager]
+) -> None:
+    """Set promo to user"""
+    promo_id = msg.text.strip()
+    promo_check_result = await promo_manager.check_promo_is_valid(promo_id)
+    match promo_check_result.result:
+        case PromoCheckValue.INVALID_PROMO_CODE:
+            await msg.answer(
+                text="Неверный формат промокода",
+                reply_markup=keyboard.cancel().as_markup()
+            )
+            return
+        case PromoCheckValue.NO_PROMO:
+            await msg.answer(
+                text="Промокод не найден, попробуйте другой",
+                reply_markup=keyboard.cancel().as_markup()
+            )
+            return
+        case PromoCheckValue.PROMO_ALREADY_ACTIVE:
+            await msg.answer(
+                text="Промокод уже использован, попробуйте другой",
+                reply_markup=keyboard.cancel().as_markup()
+            )
+            return
+        case PromoCheckValue.VALID:
+            promo = promo_check_result.promo
+        case _:
+            await send_error_message(msg, state)
+            logger.error(f"Bad result after check promo: {promo_check_result}")
+            return
 
-    # user_subscription = await subscription_storage_manager.get_user_subscription(user_id)
-    # if not user_subscription:
-    #     user_subscription = Subscription(
-    #         id=None,
-    #         user_id=user_id,
-    #         last_payment=None,
-    #         sale=100,  # TODO
-    #         until=datetime.now(),
-    #         active_links=0,
-    #         is_active=False
-    #     )
-    #     await subscription_storage_manager.create_subscription(user_subscription)
-    # else:
-    #     # TODO: logic for new subscription
-    #     ...
-    #
-    # # TODO: Payment, calculate user sale etc
-    #
-    # connection_link = await vpn_manager.add_client_with_connection_string(
-    #     user_email=f"{username}_{user_id}_{user_subscription.active_links + 1}",
-    #     connection_id=uuid.uuid4()
-    # )
-    #
-    # if connection_link is not None:
-    #     script = scripts_handler.connection_link(connection_link)
-    # else:
-    #     raise Exception
-    #
-    # if msg is not None:
-    #     await msg.answer(
-    #         text=script
-    #     )
-    #     await msg.delete()
-    # else:
-    #     await callback.answer(
-    #         text=script
-    #     )
+    apply_promo_result = await vpn_manager.apply_promo(
+        promo=promo,
+        user_id=msg.from_user.id,
+        promo_manager=promo_manager,
+        username=msg.from_user.username
+    )
+
+    match apply_promo_result.result:
+        case ApplyPromoResultValues.CREATE_NEW:
+            script = scripts_handler.successful_new_connection(
+                apply_promo_result.connection_link, apply_promo_result.new_until
+            )
+        case ApplyPromoResultValues.EXTEND_ACTIVE:
+            script = scripts_handler.successful_extend_subscription(
+                apply_promo_result.old_until,
+                apply_promo_result.new_until
+            )
+        case ApplyPromoResultValues.EXTEND_NON_ACTIVE:
+            script = scripts_handler.successful_new_connection(
+                apply_promo_result.connection_link, until=apply_promo_result.new_until
+            )
+        case _:
+            await send_error_message(msg, state)
+            logger.error(f"Bad result after apply promo: {apply_promo_result}")
+            return
+
+    await msg.answer(
+        text=script,
+        reply_markup=InlineKeyboardBuilder()
+            .button(text="Как подключить", callback_data=callbacks.CONNECTION_HELP)
+            .as_markup()
+    )
+    await state.clear()
