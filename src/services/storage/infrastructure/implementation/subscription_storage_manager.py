19a67cdefcc795e8da846aa36b722cf457d5f085
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-20 19:30:24 +0300

    STEAL-8: update subs manager

--- src/services/storage/infrastructure/implementation/subscription_storage_manager.py
+++ src/services/storage/infrastructure/implementation/subscription_storage_manager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 from charset_normalizer.md import getLogger
 from pydantic import ValidationError
@@ -5,13 +6,18 @@ from pydantic import ValidationError
 from src.core.models.subscription import Subscription
 from src.services.storage.exception import StorageManagerValidationError
 from src.services.storage.infrastructure.interfaces.i_subscription_manager import ISubscriptionStorageManager
+from src.services.storage.repository.exceptions import RepositorySubscriptionNotFound, RepositoryAlreadyExist
 
 logger = getLogger(__name__)
 
 class SubscriptionStorageManager(ISubscriptionStorageManager):
 
     async def get_user_subscription(self, user_id: int) -> Subscription | None:
-        subscription = await self.subscription_repository.get_user_subscription(user_id)
+        try:
+            subscription = await self.subscription_repository.get_user_subscription(user_id)
+        except RepositorySubscriptionNotFound:
+            return None
+
         try:
             return Subscription.model_validate(subscription) if subscription else None
         except ValidationError as err:
@@ -23,4 +29,7 @@ class SubscriptionStorageManager(ISubscriptionStorageManager):
         await self.subscription_repository.create_subscription(subscription.get_db_subscription_model())
 
     async def update_subscription(self, new_subscription: Subscription) -> None:
+        ...
+
+    async def extend_subscription(self, _id: int, new_until: datetime) -> bool:
         ...
