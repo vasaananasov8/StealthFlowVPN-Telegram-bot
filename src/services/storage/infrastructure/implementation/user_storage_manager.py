69d5b98ae7dbf57995dc720b1e2092d1eaf54241
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-20 19:29:52 +0300

    STEAL-8: update catch exc RepositoryAlreadyExist in user manager

--- src/services/storage/infrastructure/implementation/user_storage_manager.py
+++ src/services/storage/infrastructure/implementation/user_storage_manager.py
@@ -4,13 +4,17 @@ from pydantic import ValidationError
 from src.core.models.user import User
 from src.services.storage.exception import StorageManagerValidationError
 from src.services.storage.infrastructure.interfaces.i_user_storage_manager import IUserStorageManager
+from src.services.storage.repository.exceptions import RepositoryAlreadyExist
 
 logger = getLogger(__name__)
 
 class UserStorageManager(IUserStorageManager):
 
     async def create_user(self, user: User) -> None:
-        await self.user_repository.create_user(user.get_db_user_model())
+        try:
+            await self.user_repository.create_user(user.get_db_user_model())
+        except RepositoryAlreadyExist:
+            ...
 
     async def get_user(self, user_id: int) -> User:
         user = await self.user_repository.get_user(user_id)
