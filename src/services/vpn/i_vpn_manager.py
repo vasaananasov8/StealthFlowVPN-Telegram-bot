818ce857f7041542aa16a76c49921cb8f2d9d5a9
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-20 19:28:38 +0300

    STEAL-8: create i vpn manager

---
+++ src/services/vpn/i_vpn_manager.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from enum import Enum
+from typing import Any, NamedTuple
+
+from src.core.models.promo import Promo
+from src.core.models.subscription import Subscription
+from src.services.storage.infrastructure.interfaces.i_promo_manager import IPromoManager
+from src.services.storage.infrastructure.interfaces.i_subscription_manager import ISubscriptionStorageManager
+from src.services.vpn.i_vpn_repository import IVpnRepository
+
+
+class ApplyPromoResultValues(Enum):
+    CREATE_NEW = 1
+    EXTEND_ACTIVE = 2
+    EXTEND_NON_ACTIVE = 3
+
+class ApplyPromoResult(NamedTuple):
+    result: ApplyPromoResultValues
+    connection_link: str | None = None
+    old_until: datetime | None = None
+    new_until: datetime | None = None
+
+
+class IVpnManager(ABC):
+    def __init__(
+            self, vpn_repository: IVpnRepository,
+            subscription_manager: ISubscriptionStorageManager
+    ) -> None:
+        self._vpn_repository = vpn_repository
+        self._subscription_manager = subscription_manager
+
+    @staticmethod
+    def create_user_email_for_vpn(user_id: int, username: str, connection_number: int) -> str:
+        return f"{username}_{user_id}_{connection_number}"
+
+    @abstractmethod
+    async def create_connection(self) -> str:
+        ...
+
+    @abstractmethod
+    async def create_new_subscription(
+            self, subscription: Subscription,
+            user_id: str, username: str, connection_number: int
+    ) -> str:
+        ...
+
+    @abstractmethod
+    async def extend_subscription(self) -> bool:
+        ...
+
+    @abstractmethod
+    async def update_subscription(self, sub_id: int, new_data: dict[str, Any]) -> bool:
+        ...
+
+    @abstractmethod
+    async def apply_promo(
+            self, promo: Promo,
+            user_id: int, username: str,
+            promo_manager: IPromoManager
+    ) -> ApplyPromoResult:
+        ...
