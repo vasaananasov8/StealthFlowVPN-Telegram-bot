4979252d52ee7821b16b7d0e57d571e2101ef6c2
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-20 19:24:37 +0300

    STEAL-8: add callbacks

--- src/bot/ui/inline_keyboard/callbacks.py
+++ src/bot/ui/inline_keyboard/callbacks.py
@@ -1,6 +1,8 @@
 from typing import Final
 
 MAIN_MENU: Final[str] = "MAIN_MENU"
+CANCEL: Final[str] = "CANCEL"
+CONNECTION_HELP: Final[str] = "CONNECTION_HELP"
 GET_VPN: Final[str] = "GET_VPN"
 GET_USER_STATS: Final[str] = "GET_USER_STATS"
 SUPPORT: Final[str] = "SUPPORT"
