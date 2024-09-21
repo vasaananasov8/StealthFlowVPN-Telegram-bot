0354d0424f99e0bd993f3a83d5e56a89a1956dad
Nikita Shagalov <shagalovprog@gmail.com>
2024-09-21 16:30:58 +0300

    STEAL-8: refactor stats script

--- src/bot/ui/scripts/data.py
+++ src/bot/ui/scripts/data.py
@@ -8,6 +8,6 @@ SCRIPT_RU: Final[dict[str, str]] = {
                                  "Ваша ссылка для подключения: <code>{}</code>",
     "successful_extend_subscription": "Подписка успешно продлена c {} до {}",
     "stats_has_not_active_subscription": "Пока у вас нет активной подписки",
-    "stats": "Ваша подписка активна до: {}\n\n Сейчас у вас {} из {}",
+    "stats": "Ваша подписка активна до: {}\n\n Сейчас у вас {} из {} доступных активных подключение",
     "stats_active_connection_links": "Ваши ключи для подключения:\n"
 }
