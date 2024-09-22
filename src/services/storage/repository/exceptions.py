e8359bcd66dec21676164db7c08ef40216eaed6d
DKowalski25 <dshinoda25@gmail.com>
2024-09-22 23:15:16 +0300

    STEAL-19 -- Changed all exceptions.

--- src/services/storage/repository/exceptions.py
+++ src/services/storage/repository/exceptions.py
@@ -1,7 +1,4 @@
-import uuid
-
 from logging import Logger
-from functools import wraps
 from typing import Callable, Any
 from functools import wraps
 
@@ -12,66 +9,29 @@ class RepositoryException(Exception):
     ...
 
 
-class RepositoryUserNotFound(RepositoryException):
-    """An exception that occurs when the user is not found."""
-
-    def __init__(self, user_id: int):
-        self.user_id = user_id
-        super().__init__(f'User with ID {self.user_id} not found.')
-
-
-class RepositoryUserCreationError(RepositoryException):
-    """Exception for errors when creating a user"""
+class RepositoryNotFound(RepositoryException):
     ...
-
-
-class RepositorySubscriptionCreationError(RepositoryException):
-    """Exception for errors when creating a subscription"""
-    ...
-
-
-class RepositorySubscriptionNotFound(RepositoryException):
-    """An exception that occurs when a subscription is not found."""
-
-    def __init__(self, subscription_id: int):
-        self.subscription_id = subscription_id
-        super().__init__(f'Subscription with ID {self.subscription_id} not found.')
-
-
-class RepositoryConnectionCreationError(RepositoryException):
-    """Exception for errors when creating a connection"""
-    ...
-
-
-class RepositoryConnectionNotFound(RepositoryException):
-    """An exception that occurs when the connection is not found."""
-
-    def __init__(self, connection_id: int):
-        self.connection_id = connection_id
-        super().__init__(f'Connection with ID {self.connection_id} not found.')
-
-
-class RepositoryPromoCreationError(RepositoryException):
-    """Exception for errors when creating a promo"""
-    ...
-
-
-class RepositoryPromoNotFound(RepositoryException):
-    """An exception that occurs when the promo is not found."""
-
-    def __init__(self, promo_id: uuid.UUID):
-        self.promo_id = promo_id
-        super().__init__(f'Promo with ID {self.promo_id} not found.')
 
 
 def async_method_arguments_logger(logger: Logger) -> Callable:
     def decorator(method: Callable) -> Callable:
         @wraps(method)
         async def wrapper(*args: tuple[Any, ...], **kwargs: dict[str, Any]) -> Any:
-            logger.info(f"Statr handler: {method.__name__} | With arguments: {args}, {kwargs}")
-            result = await method(*args, **kwargs)
-            logger.info(f"Finish handler: {method.__name__} | With result: {result}")
-            return result
+            logger.debug(f"Statr handler: {method.__name__} | With arguments: {args}, {kwargs}")
+            try:
+                result = await method(*args, **kwargs)
+            except NoResultFound as err:
+                logger.info(f"Error: {err}")
+                raise RepositoryNotFound(err)
+            except SQLAlchemyError as err:
+                logger.info(f"Error: {err}")
+                raise RepositoryException(err)
+            except BaseException as err:
+                logger.error(f"Error: {err}")
+                raise err
+            else:
+                logger.debug(f"Finish handler: {method.__name__} | With result: {result}")
+                return result
 
         return wrapper
 
